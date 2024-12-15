c337f6688330c18f716958e2973ec0936f3373a9
ChickenGamer <chickencodes@gmail.com>
2024-12-14 22:05:23 -0500

    Added Spawn Cap
    Enemies now have a cap to spawning soall x amount don't spawn at once

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,16 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     private GameObject _player;
+    private float _speed = 10.0f;
+
+    public GameObject gameManagerObject;
+    private Vector3 _spawnPosition;
+
+    private int _health = 100;
     
     private void Start()
     {
@@ -11,9 +19,42 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if (_player != null)
+        if (_player == null) return;
+        
+        float step = _speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, step);
+    }
+
+    public void GetDamaged(int damageReceived)
+    {
+        _health -= damageReceived;
+
+        if (_health > 0) return;
+        
+        Die();
+    }
+
+    private void Die()
+    {
+        GameManager gameManager = gameManagerObject.GetComponent<GameManager>();
+        gameManager.AddPotentialSpawnPoint(_spawnPosition);
+        gameManager.DecreaseCurrentEnemiesAlive();
+        Destroy(transform.parent.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
+            // Decrease player health here
             
+            // For Testing On Death Features
+            GetDamaged(100);
         }
+    }
+
+    public void SetSpawnPosition(Vector3 spawnPosition)
+    {
+        _spawnPosition = spawnPosition;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.UI;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,13 +13,32 @@ public class GameManager : MonoBehaviour
     public Collider2D enemySpawnAreaCollider;
     public GameObject enemyPrefab;
 
-    private int _roundNumber = 0;
-    private int _enemyCount;
+    public static GameManager Instance;
+    
+    private int _roundNumber;
+    private int _maxEnemyCountPerRound;
+    private int _maxEnemyCountAtOnce;
 
     private int _currentEnemiesAlive;
+    private int _totalEnemiesSpawnedThisRound;
 
     private float _enemySpawnAreaColliderMinX, _enemySpawnAreaColliderMinY, _enemySpawnAreaColliderMaxX, _enemySpawnAreaColliderMaxY;
     private float _playerCameraMinX, _playerCameraMaxX, _playerCameraMinY, _playerCameraMaxY;
+
+    private bool _isNewRoundStarting = false;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     
     private void Start()
     {
@@ -25,96 +47,132 @@ public class GameManager : MonoBehaviour
         _enemySpawnAreaColliderMinX = enemySpawnAreaCollider.bounds.min.x;
         _enemySpawnAreaColliderMinY = enemySpawnAreaCollider.bounds.min.y;
         
+        _currentEnemiesAlive = 0;
+        
+        InitializeGrid();
         StartRound();
-    }
-
-    private void Update()
-    {
-        if (_currentEnemiesAlive <= 0 && _roundNumber != 0)
-            EndRound();
     }
 
     private void StartRound()
     {
-        _enemyCount = 4 + _roundNumber ^ 2;
-        
         _roundNumber++;
-
+        _totalEnemiesSpawnedThisRound = 0;
+        
+        _maxEnemyCountPerRound = 5 + _roundNumber * _roundNumber;        
+        
+        // Quadratic Scaling
+        // _maxEnemyCount = Mathf.CeilToInt(1 + _roundNumber * 0.5f); 
+        
+        // Log Growth
+        // _maxEnemyCount = Mathf.RoundToInt(_enemyCount * Mathf.Log(_roundNumber + 1, 2));
+        
+        // Controlled Growth with a min of 5 and cap of 20
+        _maxEnemyCountAtOnce = Mathf.Clamp(Mathf.RoundToInt(_maxEnemyCountPerRound * 0.25f), 5, 20);
+        
         waveCounterText.alpha = 1;
         waveCounterText.text = "Wave " + _roundNumber.ToString();
 
         StartCoroutine(FadeOutStartRoundText());
         SpawnEnemies();
     }
-
-    private void EndRound()
-    {
-        StartCoroutine(WaitForNextRound());
-        StartRound();
-
-    }
     
-    private void SpawnEnemies()
-{
-    int gridRows = 10;
-    int gridColumns = 10;
+    private int _gridRows = 10;
+    private int _gridColumns = 10;
 
-    float cellWidth = (_enemySpawnAreaColliderMaxX - _enemySpawnAreaColliderMinX) / gridColumns;
-    float cellHeight = (_enemySpawnAreaColliderMaxY - _enemySpawnAreaColliderMinY) / gridRows;
+    private float _cellWidth;
+    private float _cellHeight;
+    private List<Vector3> _potentialSpawnPoints = new();
 
-    List<Vector3> potentialSpawnPoints = new();
-
-    for (int x = 0; x < gridColumns; x++)
+    private void InitializeGrid()
     {
-        for (int y = 0; y < gridRows; y++)
+        _cellWidth = (_enemySpawnAreaColliderMaxX - _enemySpawnAreaColliderMinX) / _gridColumns;
+        _cellHeight = (_enemySpawnAreaColliderMaxY - _enemySpawnAreaColliderMinY) / _gridRows;
+
+        for (int x = 0; x < _gridColumns; x++)
         {
-            float spawnX = _enemySpawnAreaColliderMinX + x * cellWidth + cellWidth / 2;
-            float spawnY = _enemySpawnAreaColliderMinY + y * cellHeight + cellHeight / 2;
-            
-            Vector3 spawnPos = new Vector3(spawnX, spawnY, 0);
-            
-            Vector3 spawnViewPortPoint = Camera.main.WorldToViewportPoint(spawnPos);
-            
-            if (spawnViewPortPoint.x < 0f || 
-                spawnViewPortPoint.x > 1f || 
-                spawnViewPortPoint.y < 0f || 
-                spawnViewPortPoint.y > 1f)
-                potentialSpawnPoints.Add(new Vector3(spawnX, spawnY));
+            for (int y = 0; y < _gridRows; y++)
+            {
+                float spawnX = _enemySpawnAreaColliderMinX + x * _cellWidth + _cellWidth / 2;
+                float spawnY = _enemySpawnAreaColliderMinY + y * _cellHeight + _cellHeight / 2;
+
+                Vector3 spawnPos = new Vector3(spawnX, spawnY, 0);
+
+                Vector3 spawnViewPortPoint = Camera.main.WorldToViewportPoint(spawnPos);
+                if (spawnViewPortPoint.x < 0f || 
+                    spawnViewPortPoint.x > 1f || 
+                    spawnViewPortPoint.y < 0f || 
+                    spawnViewPortPoint.y > 1f)
+                    _potentialSpawnPoints.Add(new Vector3(spawnX, spawnY));
+            }
         }
     }
 
-    for (int i = 0; i < potentialSpawnPoints.Count; i++)
+    private bool _isSpawning;
+    public void SpawnEnemies()
     {
-        int randomIndex = Random.Range(0, potentialSpawnPoints.Count);
-        Vector3 temp = potentialSpawnPoints[i];
-        potentialSpawnPoints[i] = potentialSpawnPoints[randomIndex];
-        potentialSpawnPoints[randomIndex] = temp;
+        if (_isSpawning) return;
+        
+        _isSpawning = true;
+        
+        while (_currentEnemiesAlive < _maxEnemyCountAtOnce)
+        {
+            
+            int randomSpawnPointListPosition = Random.Range(0, _potentialSpawnPoints.Count);
+            
+            Vector3 spawnPoint = _potentialSpawnPoints[randomSpawnPointListPosition]; ;
+            GameObject newEnemy = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+            
+            GameObject sprite = newEnemy.GetComponentInChildren<SpriteRenderer>().gameObject;
+            Enemy enemyScript = sprite.AddComponent<Enemy>();
+            enemyScript.gameManagerObject = gameObject;
+            
+            enemyScript.SetSpawnPosition(spawnPoint);
+            
+            _potentialSpawnPoints.RemoveAt(randomSpawnPointListPosition);
+            
+            _totalEnemiesSpawnedThisRound += 1;
+            _currentEnemiesAlive += 1;
+        }
+        
+        if (_potentialSpawnPoints.Count == 0)
+        {
+            InitializeGrid();
+        }
+        
+        _isSpawning = false;
     }
 
-    for (int i = 0; i < _enemyCount && i < potentialSpawnPoints.Count; i++)
+    public void AddPotentialSpawnPoint(Vector3 point)
     {
-        Vector3 spawnPoint = potentialSpawnPoints[i];
-        Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
-        _currentEnemiesAlive++;
+        _potentialSpawnPoints.Add(point);
     }
-}
 
+    public void DecreaseCurrentEnemiesAlive()
+    {
+        _currentEnemiesAlive -= 1;
+        
+        if (_currentEnemiesAlive < _maxEnemyCountAtOnce && _totalEnemiesSpawnedThisRound < _maxEnemyCountPerRound)
+        {
+            SpawnEnemies();
+        }
+
+        if (_currentEnemiesAlive != 0) return;
+        
+        StartCoroutine(WaitForNextRound());
+    }
     
     IEnumerator FadeOutStartRoundText()
     {
-        int _currentFadeOutStep = 50;
-        
-        while (waveCounterText.alpha > 0)
+        while (waveCounterText.alpha > 0f)
         {
-            waveCounterText.alpha -=  1f/_currentFadeOutStep;
-            _currentFadeOutStep -= 1;
-            yield return new WaitForSeconds(.1f);
+            waveCounterText.alpha = Mathf.Lerp(waveCounterText.alpha, 0f, Time.deltaTime * .25f);
+            yield return null;
         }
     }
     IEnumerator WaitForNextRound()
     {
         yield return new WaitForSeconds(5f);
+        StartRound();
     }
-    
      
 }
