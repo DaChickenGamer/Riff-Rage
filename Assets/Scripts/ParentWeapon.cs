72cecde69ebf1179bcf24e34d0ca8c07b45dda50
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2024-12-16 23:15:45 -0500

    Weapon Order Layer
    Changed WeaponParent Script to automatically change the order layer of the weapon so that weapon looks correct.

--- Assets/Scripts/ParentWeapon.cs
+++ Assets/Scripts/ParentWeapon.cs
@@ -3,10 +3,32 @@ using UnityEngine;
 public class ParentWeapon : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SpriteRenderer characterRenderer, weaponRenderer;
     public Vector2 pointerPosition { get; set; }
 
     private void Update()
     {
-        transform.right = (pointerPosition-(Vector2)transform.position).normalized;
+        Vector2 direction = (pointerPosition -(Vector2)transform.position).normalized;
+        transform.right = direction;
+        
+        Vector2 scale = transform.localScale;
+        
+        if (direction.x < 0)
+        {
+            scale.y = -1;
+        }else if (direction.x > 0)
+        {
+            scale.y = 1;
+        }
+        transform.localScale = scale;
+
+        if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder - 1;
+        }
+        else
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
+        }
     }
 }
