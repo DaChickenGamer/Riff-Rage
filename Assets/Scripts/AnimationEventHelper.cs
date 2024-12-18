09ea3c5b3e46e91cb8f3683ea2aa5fda686fb9db
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2024-12-18 00:08:03 -0500

    animated attack
    created the animation for attacking.  Will not change weapon position when attacking.

---
+++ Assets/Scripts/AnimationEventHelper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Animatior : MonoBehaviour
+{
+    public UnityEvent OnAnimationEventTriggered;
+
+    public void TriggerEvent()
+    {
+        OnAnimationEventTriggered?.Invoke();
+    }
+}
--- Assets/Scripts/ParentWeapon.cs
+++ Assets/Scripts/ParentWeapon.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ParentWeapon : MonoBehaviour
@@ -5,9 +7,22 @@ public class ParentWeapon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public SpriteRenderer characterRenderer, weaponRenderer;
     public Vector2 pointerPosition { get; set; }
+    
+    public Animator animator;
+    public float delay = 0.3f;
+    private bool attackBlocked;
+
+    public bool IsAttacking { get; private set; }
+
+    public void ResetIsAttacking()
+    {
+        IsAttacking = false;
+    }
 
     private void Update()
     {
+        if (IsAttacking)
+            return;
         Vector2 direction = (pointerPosition -(Vector2)transform.position).normalized;
         transform.right = direction;
         
@@ -30,5 +45,21 @@ public class ParentWeapon : MonoBehaviour
         {
             weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
         }
+    }
+
+    public void Attack()
+    {
+        if (attackBlocked)
+            return;
+        animator.SetTrigger("Attack");
+        IsAttacking = true;
+        attackBlocked = true;
+        StartCoroutine(DelayAttack());
+    }
+
+    private IEnumerator DelayAttack()
+    {
+        yield return new WaitForSeconds(delay);
+        attackBlocked = false;
     }
 }
